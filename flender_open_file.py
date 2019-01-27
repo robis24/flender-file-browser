79557454b769fafbcefcfaa676dd66640157f61f
rbs <robis24@gmail.com>
2019-01-27 22:08:13 +0100

    added "O" hotkey = open new area

--- flender_open_file.py
+++ flender_open_file.py
@@ -1,6 +1,14 @@
 import bpy
 import os
 
+def mainnew(context):
+    
+    start_areas = context.screen.areas[:]
+    bpy.ops.screen.area_split(direction='VERTICAL', factor=0.5)
+    for area in context.screen.areas:
+        if area not in start_areas:
+            area.type = 'FILE_BROWSER'
+
 def main(context):
 
     
@@ -19,6 +27,19 @@ def main(context):
     os.system("xdg-open "+selectedFile)
 
 
+class OpenInNewArea(bpy.types.Operator):
+    bl_idname = "screen.openinnew_area"
+    bl_label = "Open in new area"
+    
+            
+    def execute(self, context):
+        mainnew(context)
+        return {'FINISHED'}        
+                
+    
+
+
+
 class OpenFile(bpy.types.Operator):
     """Tooltip"""
     bl_idname = "object.open_file"
@@ -35,20 +56,26 @@ class OpenFile(bpy.types.Operator):
 
 def register():
     bpy.utils.register_class(OpenFile)
+    bpy.utils.register_class(OpenInNewArea)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps.new(name="File Browser", space_type="FILE_BROWSER")
         kmi = km.keymap_items.new('object.open_file', 'LEFTMOUSE', 'DOUBLE_CLICK')
+        kml = kc.keymaps.new(name="File Browser", space_type="FILE_BROWSER")
+        kmil = kml.keymap_items.new('screen.openinnew_area', 'O', 'PRESS')
 
 def unregister():
     bpy.utils.unregister_class(OpenFile)
+    bpy.utils.unregister_class(OpenInNewArea)
     kc = bpy.context.window_manager.keyconfigs.addon
     if kc:
         km = kc.keymaps["FILE_BROWSER"]
         for kmi in km.keymap_items:
             if kmi.idname == 'object.open_file':
                 km.keymap_items.remove(kmi)
-                break
+                
+                
+                
 
 if __name__ == "__main__":
     register()
